d77f3402886b2b0b0113dd75c899d2259c84f24d
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-10-26 01:21:04 -0600

    Basic Django project
    Includes models, settings and admin

---
+++ source/corduroyproject/corduroyapp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Reports
+
+class GroomerAdmin(admin.ModelAdmin):
+    # Define the list of fields to display in the admin interface
+    list_display = ('approvalStatus', 'date','groomer', 'trailName', 'report')
+    
+    # Add search functionality for specific fields
+    search_fields = ('approvalStatus', 'date','groomer', 'trailName', 'report')
+
+    # Add filters for the age and breed fields in the sidebar
+    list_filter = ('approvalStatus', 'date','groomer', 'trailName', 'report')
+
+    # Define which fields can be clicked to view the details page
+    list_display_links = ('trailName',)
+
+    # Define how fields are displayed when editing a Report instance
+    fields = ('approvalStatus', 'date','groomer', 'trailName', 'report')
+
+# Register the model and admin class
+admin.site.register(Reports, GroomerAdmin)
--- source/corduroyproject/corduroyproject/asgi.py
+++ source/corduroyproject/corduroyproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for corduroy project.
+ASGI config for corduroyproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corduroy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corduroyproject.settings')
 
 application = get_asgi_application()
