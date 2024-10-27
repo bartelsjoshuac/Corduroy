63a47890cd788b38a0ce23d42ad9fc710e3f1d3a
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-10-26 18:43:06 -0600

    Moved to postgres
    Moved to postgres

--- source/corduroyproject/corduroyproject/settings.py
+++ source/corduroyproject/corduroyproject/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'corduroyproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+# Moving to postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'corduroydb',
+        'USER': 'postgres',     
+        'PASSWORD': 'password',      
+        'HOST': 'db',
+        'PORT': '5432',                   
     }
 }
 
