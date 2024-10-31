cef165c20745ef35d09a4535adcfb28eb67b07c6
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-10-30 22:48:58 -0600

    FIxed everything that broke messing with keys

--- source/corduroyproject/corduroyserver/models.py
+++ source/corduroyproject/corduroyserver/models.py
@@ -26,19 +26,20 @@ class Trails(models.Model):
 
 #Table of Reports   
 class Reports(models.Model):
-    # This default of false is need for when Groomers enter a report because they can not update that.
-    approvalStatus = models.BooleanField(default=False)
+    # This default of false is need for when Groomers enter a report because they can not update that. But this does not work, so I am going to go insecure and put it in the form.
+    approvalStatus = models.BooleanField(default=True)
     date = models.DateField()
     groomer = models.CharField(max_length=100)
     trailName = models.CharField(max_length=100)
     
     # Fixed the model based on feedback from exercise but this is not working, which again seems to be a postgres vs. sqlite thing
     # trailName = models.ForeignKey(Trails, on_delete=models.CASCADE)
+    # I must have tried 20 different ways to do this, trashed my database.... spent hours on it and gave up.
     
     report = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.trailName
+        return self.trailName#
 
 
 
